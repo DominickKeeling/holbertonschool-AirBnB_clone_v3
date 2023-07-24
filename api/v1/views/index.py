6a8fc23e6a7aa7ce5c0224141639a722010d89bd
dom <6157@holbertonstudents.com>
2023-07-24 13:59:27 -0500

    adding documentation hoping to fix error

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-Not sure whats happening here
+routing index file
 """
 
 from models import storage
@@ -8,10 +8,12 @@ from api.v1.views import app_views
 from flask import Flask, jsonify
 
 
+# route for obtaining the status of the API
 @app_views.route('/status', methods=['GET'])
 def status():
     return jsonify({"status": "OK"})
 
+# route for obtaining the count for all objects
 @app_views.route('/stats', methods=['GET'])
 def stats():
     classes = ['amenities', 'cities', 'places', 'reviews', 'states', 'users']
