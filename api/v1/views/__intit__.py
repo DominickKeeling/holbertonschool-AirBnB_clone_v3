28263e29d4a8d9765fbafce9ee043b96ed18af62
dom <6157@holbertonstudents.com>
2023-07-24 18:47:51 -0500

    added views/state to handle all default rest api actions

--- api/v1/views/__intit__.py
+++ api/v1/views/__intit__.py
@@ -3,8 +3,10 @@
 
 from flask import Blueprint
 
+
 # Creating a blueprint instance for API 
 app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
 
 # importing everything from index
 from api.v1.views.index import *
+from api.v1.views.states import *
