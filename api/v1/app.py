53343c6f4449de3f8f883ba5be0a055419e2c6ee
dom <6157@holbertonstudents.com>
2023-07-24 11:40:52 -0500

    added getenv to app.py

--- api/v1/app.py
+++ api/v1/app.py
@@ -3,19 +3,22 @@
 Starts a web flask application
 """
 
-from flask import Flask
+from flask import Flask, jsonify
 from api.v1.views import app_views
 from models import storage
+from os import getenv
 
 app = Flask(__name__)
 
 app.register_blueprint(app_views)
 
 @app.teardown_appcontext
-def teardown_app():
+def teardown_app(self):
     """ Method to handle app teardown """
     storage.close()
     
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, threaded=True, debug=True)
-    poop 
+    host = getenv('HBNB_API_HOST', '0.0.0.0')
+    port = int(getenv('HBNB_API_PORT', 5000))
+    app.run(host=host, port=port, threaded=True, debug=True)
+ 
