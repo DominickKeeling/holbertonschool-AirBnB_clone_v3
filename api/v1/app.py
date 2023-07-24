aeebade05e368244298f2bd4bb7665908ab13fae
dom <6157@holbertonstudents.com>
2023-07-24 11:56:33 -0500

    added documentation to app.py

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,20 +1,22 @@
 #!/usr/bin/python3
-"""
-Starts a web flask application
-"""
+"""Starts a web flask application"""
 
 from flask import Flask
 from api.v1.views import app_views
 from models import storage
 
+# Creating Flask app instance
 app = Flask(__name__)
 
+# Registers the app_view blueprint
 app.register_blueprint(app_views)
 
+# Declaring a method handler app teardown that closes the storage
 @app.teardown_appcontext
 def teardown_app():
     """ Method to handle app teardown """
     storage.close()
     
+# This starts the Flask application
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, threaded=True, debug=True)
