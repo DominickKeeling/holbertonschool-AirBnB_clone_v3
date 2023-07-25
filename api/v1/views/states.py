0b58c3a4db2c15d312e6479e235a6e02d38db2f2
tayler.coon@gmail.com <tayler.coon@gmail.com>
2023-07-25 15:21:43 -0700

    updating list

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -59,7 +59,7 @@ def put_state(state_id):
     if not request.get_json():
         abort(400, 'Not a JSON')
     for key, value in request.get_json().items():
-        if key not in ['id', 'created_at', 'updated_at']:
+        if key not in ('id', 'created_at', 'updated_at'):
             setattr(state, key, value)
     state.save()
     return jsonify(state.to_dict())
