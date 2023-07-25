59dbc1128571b209a9936899b37f045e11e305c2
F-O-P <tayler.coon@gmail.com>
2023-07-24 20:40:27 -0500

    updating documentation for index

--- api/v1/views/__intit__.py
+++ api/v1/views/__intit__.py
@@ -1,13 +1,11 @@
 #!/usr/bin/python3
 """ Init file """
-
 from flask import Blueprint
 
 
-# Creating a blueprint instance for API 
 app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
 
-# importing everything from index
+
 from api.v1.views.index import *
 from api.v1.views.states import *
 from api.v1.views.cities import *
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
-"""
-routing index file
-"""
+""" routing index file """
 
 from models import storage
 from api.v1.views import app_views
