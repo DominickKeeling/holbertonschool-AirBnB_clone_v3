7a02e639f9977174b6f533ca479eb319d3e2f0dd
F-O-P <tayler.coon@gmail.com>
2023-07-24 19:46:05 -0500

    task 4 added documentation

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -9,13 +9,15 @@ from flask import Flask, jsonify
 
 
 # route for obtaining the status of the API
-@app_views.route('/status', methods=['GET'])
+@app_views.route('/status', strict_slashes=False)
 def status():
+    """ Returns a status as json"""
     return jsonify({"status": "OK"})
 
 # route for obtaining the count for all objects
-@app_views.route('/stats', methods=['GET'])
-def stats():
+@app_views.route('/stats', strict_slashes=False)
+def count():
+    """ Returns number of each objects by type as json """
     classes = ['amenities', 'cities', 'places', 'reviews', 'states', 'users']
     stats = {}
     for cls in classes:
