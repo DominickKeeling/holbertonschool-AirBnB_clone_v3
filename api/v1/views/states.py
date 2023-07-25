04726bd7e633f1b5b3a6a1e769c1c2c27f78a9e1
tayler.coon@gmail.com <tayler.coon@gmail.com>
2023-07-25 14:12:41 -0700

    updating ssquare brackets

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -67,7 +67,7 @@ def update_state(state_id):
     if not request.get_json():
         abort(400, 'Not a JSON')
     for key, value in request.get_json().items():
-        if key not in ['id', 'created_at', 'updated_at']:
+        if key not in ('id', 'created_at', 'updated_at'):
             setattr(state, key, value)
     storage.save()
-    return jsonify(state.to_dict()), 200
+    return jsonify(state.to_dict())
