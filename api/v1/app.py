8826dfb71a282737304075c4979e9e9fbe8659f2
F-O-P <tayler.coon@gmail.com>
2023-07-24 20:14:34 -0500

    checking error:

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 """ Starts a web flask application """
-
+from models import storage
 from api.v1.views import app_views
 from flask import Flask, make_response, jsonify
-from models import storage
 from os import getenv
 
 app = Flask(__name__)
@@ -22,6 +21,8 @@ def it_borked(error):
 
 
 if __name__ == "__main__":
+    
     host = getenv('HBNB_API_HOST', default='0.0.0.0')
     port = getenv('HBNB_API_PORT', default=5000)
+
     app.run(host, int(port), threaded=True)
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -8,18 +8,17 @@ from api.v1.views import app_views
 from flask import Flask, jsonify
 
 
-# route for obtaining the status of the API
 @app_views.route('/status', strict_slashes=False)
 def status():
     """ Returns a status as json"""
     return jsonify({"status": "OK"})
 
-# route for obtaining the count for all objects
 @app_views.route('/stats', strict_slashes=False)
 def count():
     """ Returns number of each objects by type as json """
-    classes = ['amenities', 'cities', 'places', 'reviews', 'states', 'users']
-    stats = {}
-    for cls in classes:
-        stats[cls] = storage.count(cls)
-    return jsonify(stats)
+    return jsonify({"amenities": storage.count("Amenity"),
+                    "cities": storage.count("City"),
+                    "places": storage.count("Place"),
+                    "reviews": storage.count("Review"),
+                    "states": storage.count("State"),
+                    "users": storage.count("User")})
