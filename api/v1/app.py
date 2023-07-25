6f033a394e398a755fb35779ab927d7ceab54f28
tayler.coon@gmail.com <tayler.coon@gmail.com>
2023-07-25 11:53:26 -0700

    fixing app.py

--- api/v1/app.py
+++ api/v1/app.py
@@ -6,6 +6,7 @@ from api.v1.views import app_views
 from os import getenv
 
 app = Flask(__name__)
+
 app.register_blueprint(app_views)
 
 
@@ -18,11 +19,11 @@ def teardown_app(self):
 @app.errorhandler(404)
 def it_borked(error):
     """ Handles 404 errors and returns a JSON 404 status code """
-    return jsonify({"error": "Not found"}), 404
+    return jsonify(error="Not found"), 404
 
 
 if __name__ == "__main__":
     host = getenv('HBNB_API_HOST', default='0.0.0.0')
     pors = getenv('HBNB_API_PORT', default=5000)
 
-    app.run(host, int(port), threaded=True)
+    app.run(host=host, port=port, threaded=True)
